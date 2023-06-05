23a7f6c973b7f1b1b41c74d7a9831426a926e4ca
kalbek <access2kaleb@gmail.com>
2023-06-05 20:26:17 +0300

    Write the requested queries.

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+INSERT INTO animals (name, date_of_birth,weight_kg, neutered, escape_attempts)
 VALUES
     ('Agumon', '2020-02-03', 10.23, true, 0),
     ('Gabumon', '2018-11-15', 8, true, 2),
