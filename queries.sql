cd9223232e90f2ead6f7a4295f8322a81fde5ef4
kalbek <access2kaleb@gmail.com>
2023-06-06 20:53:49 +0300

    Update aminals table through transactions. result --screenshot 3--

--- queries.sql
+++ queries.sql
@@ -22,3 +22,27 @@ ROLLBACK;
 
 -- Verify if all records in the animals table still exist.
 SELECT * FROM animals;
+
+-- Start a new transaction
+BEGIN;
+
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction.
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint.
+ROLLBACK TO my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- Commit transaction.
+COMMIT;
+SELECT * FROM animals;
+
+
