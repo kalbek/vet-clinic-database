197713bec654b39780ac53d23f1d23f259fe265e
kalbek <access2kaleb@gmail.com>
2023-06-06 18:25:29 +0300

    Alter aminals table to add new column species.

--- schema.sql
+++ schema.sql
@@ -1,12 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-   id SERIAL PRIMARY KEY,
-   name VARCHAR(255),
-   date_of_birth DATE,
-   escape_attempts INT,
-   neutered BOOLEAN,
-   weight_kg DECIMAL(5,2),
-   species VARCHAR(255)
-);
+-- CREATE TABLE animals (
+--    id SERIAL PRIMARY KEY,
+--    name VARCHAR(255),
+--    date_of_birth DATE,
+--    escape_attempts INT,
+--    neutered BOOLEAN,
+--    weight_kg DECIMAL(5,2),
+--    species VARCHAR(255)
+-- );
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
 
