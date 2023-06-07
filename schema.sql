1d6ba72ec97ef021e1d4356ce347034318ce3542
kalbek <access2kaleb@gmail.com>
2023-06-07 18:25:27 +0300

    Modify animals table.

--- schema.sql
+++ schema.sql
@@ -20,3 +20,16 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+-- Drop the species column
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add the species_id column as a foreign key referencing the species table
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+-- Add the owner_id column as a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+-- Set the id column as auto-incremented primary key
+ALTER TABLE animals ALTER COLUMN id SET DEFAULT nextval('animals_id_seq');
+ALTER TABLE animals ADD PRIMARY KEY (id);
