d2e54851c3576b2056b62480d119a4a15b29844d
kalbek <access2kaleb@gmail.com>
2023-06-07 18:20:28 +0300

    Create owners table.

--- schema.sql
+++ schema.sql
@@ -1,13 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-   id SERIAL PRIMARY KEY,
-   name VARCHAR(255),
-   date_of_birth DATE,
-   escape_attempts INT,
-   neutered BOOLEAN,
-   weight_kg DECIMAL(5,2)
+-- CREATE TABLE animals (
+--    id SERIAL PRIMARY KEY,
+--    name VARCHAR(255),
+--    date_of_birth DATE,
+--    escape_attempts INT,
+--    neutered BOOLEAN,
+--    weight_kg DECIMAL(5,2)
+-- );
+-- ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL
 );
-ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
 
 
