3c8e16cc5e9e6a9c2154c3f87546b932935d1291
kalbek <access2kaleb@gmail.com>
2023-06-12 22:41:49 +0300

    Use explain analyze to test queries.

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 -- query to check performance
 -- explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
 
-SELECT COUNT(*) FROM visits where animal_id = 4;
-SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+explain analyze SELECT * FROM visits where vet_id = 2;
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
 
