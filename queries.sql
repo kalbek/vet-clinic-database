2ee9e8bd2f180176532f61c1658ea125d89fbf66
kalbek <access2kaleb@gmail.com>
2023-06-12 22:39:59 +0300

    Add test queiries to test performance.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,8 @@
 
 -- query to check performance
-explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
+-- explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
 
-
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
 
