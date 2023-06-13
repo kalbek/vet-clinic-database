3780ff35e9b50d0c1391d81c25c5941e83f76c4b
eudondian <eudondian@gmail.com>
2023-06-13 09:17:12 +0100

    Updated invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,12 @@ CREATE TABLE patients (
   name VARCHAR(255),
   date_of_table DATE
 );
+
+CREATE TABLE invoices (
+  id SERIAL PRIMARY INTEGER,
+  totat_amount DECIMAL(10,2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT
+);
+
