e67867fa35e67db2703e97d05281cb50afc5a537
kalbek <access2kaleb@gmail.com>
2023-06-13 13:59:33 +0300

    Create indexes on foreing key columns.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -39,5 +39,13 @@ CREATE TABLE invoice_items (
   treatment_id INT REFERENCES treatments(id)
 );
 
+CREATE INDEX medical_histories_patient_id_idx ON medical_histories (patient_id);
+CREATE INDEX medical_history_treatments_medical_history_id_idx ON medical_history_treatments (medical_history_id);
+CREATE INDEX medical_history_treatments_treatment_id_idx ON medical_history_treatments (treatment_id);
+CREATE INDEX invoices_medical_history_id_idx ON invoices (medical_history_id);
+CREATE INDEX invoice_items_invoice_id_idx ON invoice_items (invoice_id);
+CREATE INDEX invoice_items_treatment_id_idx ON invoice_items (treatment_id);
+
+
 
 
