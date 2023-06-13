65c33e8b734b5074863b881d025632930471f25c
kalbek <access2kaleb@gmail.com>
2023-06-13 12:52:03 +0300

    Rearrange table creation statements, clean code ready for submission.

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,34 +9,35 @@ CREATE TABLE medical_histories (
   admitted_at TIMESTAMP,
   patient_id INT REFERENCES patients(id),
   status VARCHAR (50) 
- );
+);
 
- CREATE TABLE medical_history_treatments (
+ CREATE TABLE treatments (
+  id SERIAL PRIMARY KEY,
+  type VARCHAR (50),
+  name VARCHAR (40)
+);
+
+CREATE TABLE medical_history_treatments (
     id SERIAL PRIMARY KEY,
     medical_history_id INT REFERENCES medical_histories(id),
     treatment_id INT REFERENCES treatments(id)
 );
 
- CREATE TABLE treatments (
-  id SERIAL PRIMARY INTEGER,
-  type VARCHAR (50),
-  name VARCHAR (40)
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  totat_amount DECIMAL(10,2),
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT UNIQUE REFERENCES medical_histories(id)
 );
 
 CREATE TABLE invoice_items (
-  id SERIAL PRIMARY INTEGER,
+  id SERIAL PRIMARY KEY,
   unit_price DECIMAL(10,2),
   quantity INT,
   invoice_id INT REFERENCES invoices(id),
   treatment_id INT REFERENCES treatments(id)
 );
 
-CREATE TABLE invoices (
-  id SERIAL PRIMARY INTEGER,
-  totat_amount DECIMAL(10,2),
-  generated_at TIMESTAMP,
-  payed_at TIMESTAMP,
-  medical_history_id INT UNIQUE REFERENCES medical_histories(id),
-);
 
 
